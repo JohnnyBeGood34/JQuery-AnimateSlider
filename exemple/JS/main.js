947e25adde1111a21e7942ec988a04b87c9aea75
John <JohnnyBeGood34@users.noreply.github.com>
2014-06-11 10:11:26 +0200

    Update main.js

--- exemple/JS/main.js
+++ exemple/JS/main.js
@@ -1,3 +1,14 @@
 $(function(){
-
+  var screenHeight = $(window).height();
+  //Height of slider
+  $("#home-slider").css("height",screenHeight);
+  //Height of img group
+  $("#home-slider .image-right").css("height",$("#home-slider").height());
+  
+  //Height of navigations buttons
+  var heightOfNavButtons = $("#home-slider .nav-arrows").height(),
+      marginNavButtons = screenHeight - (heightOfNavButtons * 3.5);
+  
+  $("#home-slider .nav-arrows").css("margin-top",marginNavButtons);
+  
 });
